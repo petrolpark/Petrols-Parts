b513886ae13a8eeab1aa527bcaf7180a32640811
Petrolpark <p3tr0lp4rk@gmail.com>
2025-04-05 17:24:25 +0100

    🔧 DCS wrenching fix

--- src/main/java/com/petrolpark/petrolsparts/content/double_cardan_shaft/DoubleCardanShaftBlock.java
+++ src/main/java/com/petrolpark/petrolsparts/content/double_cardan_shaft/DoubleCardanShaftBlock.java
@@ -171,6 +171,11 @@ public class DoubleCardanShaftBlock extends DirectionalAxisKineticBlock implemen
 	};
 
     @Override
+    protected boolean areStatesKineticallyEquivalent(BlockState oldState, BlockState newState) {
+        return false;
+    };
+
+    @Override
     public Class<DoubleCardanShaftBlockEntity> getBlockEntityClass() {
         return DoubleCardanShaftBlockEntity.class;
     };
