764acdc923cdd936b74d891c4b8f3ddd054e58b5
Petrolpark <p3tr0lp4rk@gmail.com>
2026-04-21 14:35:33 +0100

    🥣 Remove duplicate corner shaft recipes

--- src/main/java/com/petrolpark/petrolsparts/core/PetrolsPartsRegistrate.java
+++ src/main/java/com/petrolpark/petrolsparts/core/PetrolsPartsRegistrate.java
@@ -1,6 +1,6 @@
 package com.petrolpark.petrolsparts.core;
 
-import com.petrolpark.PetrolparkRegistrate;
+import com.petrolpark.compat.create.core.AbstractPetrolparkCreateRegistrate;
 import com.petrolpark.core.registrate.builder.PetrolparkItemBuilder;
 import com.petrolpark.petrolsparts.PetrolsParts;
 import com.simibubi.create.foundation.data.CreateBlockEntityBuilder;
@@ -12,18 +12,17 @@ import com.tterrag.registrate.util.nullness.NonNullFunction;
 
 import net.createmod.catnip.lang.FontHelper.Palette;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-public final class PetrolsPartsRegistrate extends PetrolparkRegistrate {
+public final class PetrolsPartsRegistrate extends AbstractPetrolparkCreateRegistrate<PetrolsPartsRegistrate> {
 
     public PetrolsPartsRegistrate() {
         super(PetrolsParts.MOD_ID);
     };
 
     @Override
-    public <T extends Item> PetrolparkItemBuilder<T, PetrolparkRegistrate> item(String name, NonNullFunction<Properties, T> factory) {
-        final PetrolparkItemBuilder<T, PetrolparkRegistrate> builder = super.item(name, factory);
+    public <T extends Item> PetrolparkItemBuilder<T, PetrolsPartsRegistrate> item(String name, NonNullFunction<Item.Properties, T> factory) {
+        final PetrolparkItemBuilder<T, PetrolsPartsRegistrate> builder = super.item(name, factory);
         builder.onRegister(item -> TooltipModifier.REGISTRY.register(item, new ItemDescription.Modifier(item, Palette.STANDARD_CREATE).andThen(TooltipModifier.mapNull(KineticStats.create(item)))));
         return builder;
     };
